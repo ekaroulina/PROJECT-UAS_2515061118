319f935b37aeec91db78f4749ad2cd362950bbef
ekaroulina <ekaroulina@gmail.com>
2026-06-13 19:45:45 +0700

    Add files via upload

---
+++ matriks.py
@@ -0,0 +1,36 @@
+import eka118
+
+# Definisikan dua matriks 3x3
+A = [[1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+
+B = [[9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]]
+
+# Perkalian matriks 3x3 menggunakan fungsi khusus
+print("Perkalian Matriks 3x3")
+hasil_kali = eka118.kali_matriks_3x3(A, B)
+for row in hasil_kali:
+    print(row)
+
+# Penjumlahan
+print("\nPenjumlahan Matriks")
+hasil_tambah = eka118.tambah(A, B)
+for row in hasil_tambah:
+    print(row)
+
+# Determinan
+print("\nDeterminan Matriks A")
+det_A = eka118.determinan(A)
+print(det_A)
+
+# Invers (jika matriks non-singular, coba matriks lain)
+C = [[4, 7, 2],
+    [3, 6, 1],
+    [2, 5, 3]]
+print("\nInvers Matriks C")
+inv_C = eka118.invers(C)
+for row in inv_C:
+    print([round(x, 2) for x in row]) 
